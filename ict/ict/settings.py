d298fce9c8ea49ab1478bc9dbbd2d02c6cad2428
Md. Ahasan Habib Shanto <99389726+mash2022@users.noreply.github.com>
2023-03-21 20:20:44 +0600

    image

--- ict/ict/settings.py
+++ ict/ict/settings.py
@@ -120,13 +120,17 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
+
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
