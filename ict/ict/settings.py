0dd2e53de5a39aedb55f1198152c32cf322d11b9
mash2022 <phmahs86@gmail.com>
2023-04-04 13:53:12 +0600

    okay

--- ict/ict/settings.py
+++ ict/ict/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-7lir7pab+o(td@gq4p1%ycc6w61nhgu&)r3qfjl8!z&a486i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
