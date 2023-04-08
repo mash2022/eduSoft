94388f50b674c73edb6358372d0e9e031b7eb332
Md. Ahasan Habib Shanto <99389726+mash2022@users.noreply.github.com>
2023-04-08 21:16:23 +0600

    carousel

--- ict/courses/models.py
+++ ict/courses/models.py
@@ -52,7 +52,7 @@ class Image(models.Model):
     def save(self):
         super().save()
         img=Im.open(self.photo.path)
-        if img.height>300 or img.width>300:
+        if img.height>300 or img.width>150:
             output_size=(300, 300)
             img.thumbnail(output_size)
             img.save(self.photo.path)
