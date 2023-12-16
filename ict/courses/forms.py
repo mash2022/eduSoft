f3240946f038791876939b13f85be64a157c4b5e
Md. Ahasan Habib Shanto <99389726+mash2022@users.noreply.github.com>
2023-12-16 13:08:56 +0600

    okay

--- ict/courses/forms.py
+++ ict/courses/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import *
-from django.forms import DateField, TextInput, EmailInput, Textarea
+from django.forms import ModelForm, DateField, TextInput, EmailInput, Textarea
 
 class ImageUploadForm(forms.ModelForm):
     class Meta:
@@ -41,7 +41,7 @@ class Admission_form(forms.ModelForm):
         widgets={
             'student_name':TextInput(attrs={
                 'class':'form-control required',
-                'style':'max_width: 50px;',
+                'style':'max_width: 20px;',
                 'placeholder':'Name of student'
             }),
             'father_name':TextInput(attrs={
@@ -56,13 +56,25 @@ class Admission_form(forms.ModelForm):
             })
         }
 
-class RmpLmfForm(forms.ModelForm):
+class RmpLmfForm(ModelForm):
     class Meta:
         model=RmpLmf
         fields=['student_name_ban', 'student_name_eng', 'father_or_hus_name_ban', 'father_or_hus_name_eng', 'village_ban', 'village_eng', 'post_office_ban', 'post_office_eng', 'upozila_ban', 'upozila_eng', 'district_ban', 'district_eng', 'religion', 'nid', 'mobile', 'nationality', 'blood_group', 'marrietal_status', 'age', 'edu_qualification', 'is_pharmacy_have', 'pharmacy_name_address', 'past_training_name']  
         widgets={
             'student_name_ban':TextInput(attrs={
+                'class':'form-control',
+                'style':'max_width: 300px;',
+                'placeholder':'Name'}),
+            'student_name_eng':TextInput(attrs={
+                'class':'form-control',
+                'style':'max_width: 300px;',
+                'placeholder':'Name'}),
+            'father_or_hus_name_ban':TextInput(attrs={
                 'class':'form-control required',
                 'style':'max_width: 50px;',
-                'placeholder':'Student name in Bangla'}),
+                'placeholder':'Name'}),
+            'father_or_hus_name_eng':TextInput(attrs={
+                'class':'form-control required',
+                'style':'max_width: 50px;',
+                'placeholder':'Name'}),
         }      
