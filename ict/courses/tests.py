6fa64aa075575093abf51d2120354c455bd34099
Md. Ahasan Habib Shanto <99389726+mash2022@users.noreply.github.com>
2024-01-17 22:32:29 +0600

    okay

--- ict/courses/tests.py
+++ ict/courses/tests.py
@@ -3,3 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 #https://www.geeksforgeeks.org/generate-a-pdf-in-django/
 #https://www.geeksforgeeks.org/django-sign-up-and-login-with-confirmation-email-python/?ref=lbp
+#https://ordinarycoders.com/blog/article/django-user-register-login-logout
